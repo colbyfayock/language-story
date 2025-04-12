c9e570bc45721f2e67062c3c486c5ef59b56aafa
Colby Fayock <fayock@gmail.com>
2025-04-11 23:43:09 -0400

    logo, favicon

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,13 +1,17 @@
 import Container from "@/components/Container";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
+import LanguageStoryIcon from "@/components/svg/LanguageStoryIcon";
 
 const Header = () => {
   return (
     <header>
       <Container className="flex items-center justify-between py-5">
         <p className="font-bold text-lg">
-          <Link href="/">Language Story</Link>
+          <Link href="/">
+            <LanguageStoryIcon className="w-9 h-auto mr-2" />
+            Language Story
+          </Link>
         </p>
         <ul className="flex items-center gap-4">
           <SignedOut>
